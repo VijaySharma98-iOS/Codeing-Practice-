688e9414f1a434af3508d9401dcf9fc2999c67f8
Vijay Sharma <vijay.sharma.tdv@gmail.com>
2026-01-30 09:21:50 +0530

    Add initial ECommerce iOS app project
    Set up a new Xcode project for the ECommerce app, including project configuration, main storyboard, launch screen, Core Data model, asset catalogs, and initial view controllers (AppDelegate, SceneDelegate, ViewController, ProfileViewController).

---
+++ ECommerce/ECommerce/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ECommerce
+//
+//  Created by Vijay Sharma on 07/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
