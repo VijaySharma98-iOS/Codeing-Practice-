ebf67b329608bf2ba7b193714ad4d7c627c44b8a
Vijay Sharma <vijay.sharma.tdv@gmail.com>
2026-01-30 10:02:36 +0530

    Add Fibonacci sequence function to playground
    Introduced a new function to generate and print the Fibonacci sequence up to n terms. The previous print statement for the reversed string was commented out.

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -7,4 +7,28 @@ var reserse = ""
 for char in str {
     reserse = String(char) + reserse
 }
-print(reserse)
+//print(reserse)
+
+
+func fibonacci(n: Int) {
+    var arr:[Int] = []
+    
+    var a = 0
+    var b = 1
+    
+    for value in 0..<n {
+        if value == 0 {
+            arr.append(a)
+        } else if value == 1 {
+            arr.append(b)
+        } else {
+            var c = a + b
+            arr.append(c)
+            a = b
+            b = c
+        }
+    }
+    print(arr)
+}
+
+fibonacci(n: 10)
